b57b27d93527498f84cbcb44cd2cca8dc949d477
Korotenko Alex <alexey.korotenko90@gmail.com>
2021-10-24 13:11:51 +0300

    commit

--- index.js
+++ index.js
@@ -1,36 +1,21 @@
+const promotionTimer = new Date('Dec 19,2021 00:00:00');
 
-class CountdownTimer{
-    
-}
+const timer = document.querySelector('.js-timer');
+
+setInterval(() => {
+    const dateNow = Date.now();
+
+    const distance = promotionTimer - dateNow;
+const days =  Math.floor(distance/(24*60*60*1000));
+// console.log(days);
+const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+const mins = (Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)));
+// console.log(mins);
+const secs = Math.floor((distance % (1000 * 60)) / 1000);
+timer.innerHTML= `days:${days} hours:${hours} mins:${mins} secs: ${secs}`;
+}, 1000);
 
 
-// new CountdownTimer({
-//   selector: '#timer-1',
-//   targetDate: new Date('Jul 17, 2019'),
-// });
 
 
-/*
- * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
- * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
- */
-const days = Math.floor(time / (1000 * 60 * 60 * 24));
-
-/*
- * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
- * остатка % и делим его на количество миллисекунд в одном часе
- * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
- */
-const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-
-/*
- * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
- * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
- */
-const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-
-/*
- * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
- * миллисекунд в одной секунде (1000)
- */
-const secs = Math.floor((time % (1000 * 60)) / 1000);
